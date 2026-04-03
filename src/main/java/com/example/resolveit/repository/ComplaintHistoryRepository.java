530d72ccc6c4d1b5047898a387c509fa37fb3cac
Morampudi Satwika <morampudisatwika@gmail.com>
2026-04-03 21:20:50 +0530

    Delete src/main directory

--- src/main/java/com/example/resolveit/repository/ComplaintHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.resolveit.repository;
-
-import com.example.resolveit.model.ComplaintHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ComplaintHistoryRepository extends JpaRepository<ComplaintHistory, Integer> {
-    List<ComplaintHistory> findByComplaintIdOrderByUpdatedAtDesc(int complaintId);
-}
